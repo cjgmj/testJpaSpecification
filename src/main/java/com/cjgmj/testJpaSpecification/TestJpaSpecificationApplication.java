8059c831b29287b49d36eef322023a0644747075
cjgmj <cjgmj@hotmail.com>
2019-07-28 11:17:10 +0200

    Proyecto base

---
+++ src/main/java/com/cjgmj/testJpaSpecification/TestJpaSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.cjgmj.testJpaSpecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJpaSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJpaSpecificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjgmj/testJpaSpecification/TestJpaSpecificationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cjgmj.testJpaSpecification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestJpaSpecificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
